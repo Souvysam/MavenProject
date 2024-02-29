03f6a3d544f9355c77cc78c26cf90d336250807f
souravk2 <sourav0212buddy@gmail.com>
2024-02-29 12:53:06 +0530

    first commit

---
+++ src/main/java/com/cisco/simplemavenproject/App.java
@@ -0,0 +1,14 @@
+package com.cisco.simplemavenproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "welcome to the world of jenkins!" );
+        System.out.println( "helo from project" );
+    }
+}
